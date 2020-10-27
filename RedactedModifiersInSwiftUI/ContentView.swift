4cc94320391523f3602ed53f8d3ef5eef9f99d98
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-27 19:10:34 +0200

    Initial Commit

---
+++ RedactedModifiersInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RedactedModifiersInSwiftUI
+//
+//  Created by Ramill Ibragimov on 27.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RedactedModifiersInSwiftUI/RedactedModifiersInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedactedModifiersInSwiftUIApp.swift
+//  RedactedModifiersInSwiftUI
+//
+//  Created by Ramill Ibragimov on 27.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct RedactedModifiersInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
